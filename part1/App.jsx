4d56d9f8762fd9deb28c9d64d8fff124a73f1f02
laurikor00 <84325911+laurikor00@users.noreply.github.com>
2024-06-25 13:24:39 +0300

    Add files via upload

---
+++ part1/App.jsx
@@ -0,0 +1,65 @@
+import { useState } from 'react'
+
+const Button = ({ handleClick, text }) => {
+  return <button onClick={handleClick}>{text}</button>
+}
+
+const StatisticLine = ({ text, value }) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  )
+}
+
+const Statistics = ({ good, neutral, bad, total, average, positivePercentage }) => {
+  if (total === 0) {
+    return <p>No feedback given</p>;
+  }
+  return (
+    <table>
+      <tbody>
+        <StatisticLine text="good" value={good} />
+        <StatisticLine text="neutral" value={neutral} />
+        <StatisticLine text="bad" value={bad} />
+        <StatisticLine text="all" value={total} />
+        <StatisticLine text="average" value={average.toFixed(1)} />
+        <StatisticLine text="positive" value={`${positivePercentage.toFixed(1)}%`} />
+      </tbody>
+    </table>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  /*const [all, setAll] = useState(0)
+  const [avg, setAvg] = useState(0)
+  const [per, setPer] = useState(0)*/
+  const total = good + neutral + bad
+  const average = total === 0 ? 0 : (good - bad) / total
+  const positivePercentage = total === 0 ? 0 : (good / total) * 100
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button handleClick={() => setGood(good + 1)} text="good" />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button handleClick={() => setBad(bad + 1)} text="bad" />
+      <h1>statistics</h1>
+      <Statistics 
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        total={total}
+        average={average}
+        positivePercentage={positivePercentage}
+      />
+    </div>
+  )
+}
+
+export default App
