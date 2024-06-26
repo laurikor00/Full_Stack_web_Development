eb48ee9621217ee5f9a6648c72d28f9fc55c8ae3
laurikor00 <84325911+laurikor00@users.noreply.github.com>
2024-06-26 13:35:54 +0300

    Add files via upload

---
+++ part2/CourseInfo/Course.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+const Header = ({ courseName }) => {
+  return <h1>{courseName}</h1>;
+}
+
+const Part = ({ part }) => {
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>
+  )
+}
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map(part => (
+        <Part key={part.id} part={part} />
+      ))}
+    </div>
+  )
+}
+
+const Total = ({ parts }) => {
+    const total = parts.reduce((sum, part) => sum + part.exercises, 0);
+    return <p><strong>Total of {total} exercises</strong></p>;
+  }
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header courseName={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+export default Course;
