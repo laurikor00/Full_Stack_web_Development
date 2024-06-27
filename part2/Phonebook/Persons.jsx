31e3884976ce82d11bc2a9b4637ed21578e74703
laurikor00 <84325911+laurikor00@users.noreply.github.com>
2024-06-27 14:42:10 +0300

    Update Persons.jsx

--- part2/Phonebook/Persons.jsx
+++ part2/Phonebook/Persons.jsx
@@ -1,12 +1,14 @@
-const Persons = ({ persons }) => {
+const Persons = ({ persons, handleDelete }) => {
     return (
       <ul>
         {persons.map((person, index) => (
-          <li key={index}>{person.name} {person.number}</li>
+          <li key={index}>
+            {person.name} {person.number}
+            <button onClick={() => handleDelete(person.id)}>delete</button>
+          </li>
         ))}
       </ul>
     );
   };
   
   export default Persons;
-  
