adb76b8763985277150d37fe6e798a4bd530655f
laurikor00 <84325911+laurikor00@users.noreply.github.com>
2024-07-17 11:22:17 +0300

    Update blogs.js

--- part4/bloglist/backend/controllers/blogs.js
+++ part4/bloglist/backend/controllers/blogs.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken')
 const router = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
-const userExtractor = require('../utils/middleware').userExtractor
+const middleware = require('../utils/middleware')
 
 router.get('/', async (request, response) => {
   const blogs = await Blog
@@ -10,32 +10,8 @@ router.get('/', async (request, response) => {
 
   response.json(blogs)
 })
-/*
-router.post('/', userExtractor, async (request, response) => {
-  const blog = new Blog(request.body)
 
-  const user = request.user
-
-  if (!user ) {
-    return response.status(403).json({ error: 'user missing' })
-  }  
-
-  if (!blog.title || !blog.url ) {
-    return response.status(400).json({ error: 'title or url missing' })
-  }   
-
-  blog.likes = blog.likes | 0
-  blog.user = user
-  user.blogs = user.blogs.concat(blog._id)
-
-  await user.save()
-
-  const savedBlog = await blog.save()
-
-  response.status(201).json(savedBlog)
-})*/
-
-router.post('/', userExtractor, async (request, response) => {
+router.post('/', middleware.userExtractor, async (request, response) => {
   const body = request.body;
 
   if (!request.user || !request.user.id) {
@@ -63,26 +39,23 @@ router.post('/', userExtractor, async (request, response) => {
   }
 });
 
-router.delete('/:id', userExtractor, async (request, response) => {
-  const user = request.user
-
-  const blog = await Blog.findById(request.params.id)
-  if (!blog) {
-    return response.status(204).end()
+router.delete('/:id', middleware.userExtractor, async (request, response) => {
+  const user = request.user;
+  try {
+    const blog = await Blog.findById(request.params.id);
+    if (!blog) {
+      return response.status(404).json({ error: 'Blog not found' });
+    }
+    if (blog.user.toString() !== user.id.toString()) {
+      return response.status(401).json({ error: 'Unauthorized to delete this blog' });
+    }
+    await blog.deleteOne(); // Use deleteOne method to delete the blog
+    response.status(204).end();
+  } catch (error) {
+    console.error('Failed to delete blog', error);
+    response.status(500).json({ error: 'Failed to delete blog' });
   }
-
-  if ( user.id.toString() !== blog.user.toString() ) {
-    return response.status(403).json({ error: 'user not authorized' })
-  }
-
-  await blog.deleteOne()
-
-  user.blogs = user.blogs.filter(b => b._id.toString() !== blog._id.toString())
-
-  await user.save()
-
-  response.status(204).end()
-})
+});
 
 router.put('/:id', async (request, response) => {
   const body = request.body;
