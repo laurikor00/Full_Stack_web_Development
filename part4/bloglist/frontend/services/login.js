f186c985f5117832d832ca1b29288e6853c0e746
laurikor00 <84325911+laurikor00@users.noreply.github.com>
2024-07-17 09:45:18 +0300

    Add files via upload

---
+++ part4/bloglist/frontend/services/login.js
@@ -0,0 +1,10 @@
+// src/services/login.js
+import axios from 'axios';
+const baseUrl = '/api/login';
+
+const login = async (username, password) => {
+  const response = await axios.post(baseUrl, { username, password });
+  return response.data;
+};
+
+export default { login };
