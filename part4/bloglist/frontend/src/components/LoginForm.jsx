b653676dec6c2cedba6a9c8e3f2176a5b80765ad
laurikor00 <84325911+laurikor00@users.noreply.github.com>
2024-07-18 15:39:10 +0300

    Update LoginForm.jsx

--- part4/bloglist/frontend/src/components/LoginForm.jsx
+++ part4/bloglist/frontend/src/components/LoginForm.jsx
@@ -1,5 +1,4 @@
-// src/components/LoginForm.js
-// src/components/LoginForm.js
+
 import React from 'react';
 
 const LoginForm = ({ handleLogin, username, password, setUsername, setPassword }) => {
@@ -14,6 +13,7 @@ const LoginForm = ({ handleLogin, username, password, setUsername, setPassword }
         username
         <input
           type="text"
+          data-testid='username'
           value={username}
           onChange={({ target }) => setUsername(target.value)}
         />
@@ -21,6 +21,7 @@ const LoginForm = ({ handleLogin, username, password, setUsername, setPassword }
       <div>
         password
         <input
+          data-testid='password'
           type="password"
           value={password}
           onChange={({ target }) => setPassword(target.value)}
@@ -32,41 +33,3 @@ const LoginForm = ({ handleLogin, username, password, setUsername, setPassword }
 };
 
 export default LoginForm;
-
-
-
-/*import { useState } from 'react';
-
-const LoginForm = ({ handleLogin }) => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    handleLogin({ username, password });
-  };
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        Username
-        <input
-          type="text"
-          value={username}
-          onChange={({ target }) => setUsername(target.value)}
-        />
-      </div>
-      <div>
-        Password
-        <input
-          type="password"
-          value={password}
-          onChange={({ target }) => setPassword(target.value)}
-        />
-      </div>
-      <button type="submit">Login</button>
-    </form>
-  );
-};
-
-export default LoginForm;*/
